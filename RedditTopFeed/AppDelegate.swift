687e72baad5619d56592099bc6e002aaf7c7f37c
Volodymyr Sakhan <free.divbyzero@gmail.com>
2020-11-24 11:13:19 +0200

    init commit

---
+++ RedditTopFeed/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  RedditTopFeed
+//
+//  Created by  Volodymyr Sakhan on 24.11.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ RedditTopFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditTopFeed
+//
+//  Created by  Volodymyr Sakhan on 24.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
